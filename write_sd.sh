38387acb11aeb2260b1db7be0fd64e4e4618b98a
Denis Marin <dvmarin@luxoft.com>
2021-06-04 19:41:44 +0300

    Add convenience script for SD creation

---
+++ write_sd.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+WORKING_DIR=`dirname $0`
+
+if [ -z "${1}" ]; then
+    echo "usage: $0 /dev/sd?"
+    echo "example: $0 /dev/sdb"
+    exit 1
+fi
+
+
+# unmount partitions if mounted
+sudo umount ${1}1
+sudo umount ${1}2
+sudo umount ${1}3
+sudo umount ${1}4
+
+sudo dd if=$WORKING_DIR/IMAGE/output.img of=${1} bs=4M
